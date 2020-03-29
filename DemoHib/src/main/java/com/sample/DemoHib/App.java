4f45203ace0ac3b0c5acd0213c62f13d769dd164
krishna5555 <akshathk55@gmail.com>
2020-03-29 11:13:47 +0530

    Added hibernate configuration file

--- DemoHib/src/main/java/com/sample/DemoHib/App.java
+++ DemoHib/src/main/java/com/sample/DemoHib/App.java
@@ -1,5 +1,10 @@
 package com.sample.DemoHib;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,19 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Alien alien = new Alien();
+        alien.setAid(101);
+        alien.setAname("ronaldo");
+        alien.setColor("Blue");
+        
+        Configuration con = new Configuration().configure().addAnnotatedClass(Alien.class);
+
+        SessionFactory sf = con.buildSessionFactory();
+        
+        Session session = sf.openSession();
+        
+        Transaction tx = session.beginTransaction();
+        session.save(alien);
+        tx.commit();
     }
 }
